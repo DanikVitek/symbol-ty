afd3517147904c09c7a3772d6a683b5199895dcb
Danik Vitek <x3665107@gmail.com>
2025-02-11 13:27:03 +0200

    Implement `Chars` iterator. Remove needless broken seal from the `Symbol` trait

--- examples/foreign_crate/src/main.rs
+++ examples/foreign_crate/src/main.rs
@@ -1,7 +1,11 @@
+use core::fmt;
+
 use symbol_ty::Symbol;
 
 fn main() {
     print_symbol(<Symbol!("Hello, world!")>::new());
+    println!();
+    print_symbol(foo_bar);
 
     assert_eq!(
         <Symbol!("Hello, world!")>::new(),
@@ -9,8 +13,73 @@ fn main() {
     );
 }
 
-fn print_symbol(s: impl Symbol) {
+fn print_symbol<S: Symbol>(s: S)
+where
+    <S as Symbol>::Chars: DoubleEndedIterator,
+{
     println!("{}", s);
     println!("{:?}", s.to_string());
     println!("{:?}", s);
+    println!("{:?}", S::chars().collect::<Vec<_>>());
+    println!("{:?}", S::chars().rev().collect::<Vec<_>>());
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
+#[allow(non_camel_case_types)]
+struct foo_bar;
+
+impl Symbol for foo_bar {
+    type Chars = Chars_foo_bar;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn chars() -> Self::Chars {
+        Chars_foo_bar {
+            pos: 0,
+            end: "foo_bar".len(),
+        }
+    }
+}
+
+impl fmt::Display for foo_bar {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("foo_bar")
+    }
+}
+
+#[allow(non_camel_case_types)]
+struct Chars_foo_bar {
+    pos: usize,
+    end: usize,
+}
+
+impl Iterator for Chars_foo_bar {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let res = *b"foo_bar"[..self.end].get(self.pos)? as char;
+        self.pos += 1;
+        Some(res)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+impl ExactSizeIterator for Chars_foo_bar {
+    fn len(&self) -> usize {
+        self.end - self.pos
+    }
+}
+
+impl DoubleEndedIterator for Chars_foo_bar {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        let res = *b"foo_bar"[..self.end - self.pos].get(self.end.checked_sub(1)?)? as char;
+        self.end -= 1;
+        Some(res)
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 #![no_std]
 
-use core::{fmt, fmt::Write, hash::Hash};
+use core::{fmt, fmt::Write, hash::Hash, iter::FusedIterator};
 
 pub use symbol_ty_macro::Symbol;
 
@@ -16,36 +16,67 @@ pub struct Nil;
 
 /// A symbol, which is a type-level string.
 pub trait Symbol: fmt::Display + fmt::Debug + Default + Eq + Ord + Copy + Sized + Hash {
+    type Chars: Iterator<Item = char>;
+
     /// Get an instance of the symbol.
     fn new() -> Self;
 
-    #[doc(hidden)]
-    fn _sealed(_: __private::Seal) {}
-}
-
-mod __private {
-    pub struct Seal;
+    /// Get an iterator over the characters of the symbol.
+    fn chars() -> Self::Chars;
 }
 
 impl Symbol for Nil {
+    type Chars = core::iter::Empty<char>;
+
+    #[inline(always)]
     fn new() -> Self {
-        Self
+        Self::new()
+    }
+
+    #[inline(always)]
+    fn chars() -> Self::Chars {
+        core::iter::empty()
     }
 }
 
 impl<const C: char, Tail: Symbol> Symbol for Cons<C, Tail> {
+    type Chars = Chars<C, <Tail as Symbol>::Chars>;
+
     fn new() -> Self {
         Self(Tail::new())
+    }
+
+    fn chars() -> Self::Chars {
+        Chars {
+            used_c: false,
+            tail: Tail::chars(),
+        }
+    }
+}
+
+impl<const C: char, Tail: Symbol> Cons<C, Tail> {
+    #[inline(always)]
+    pub fn new() -> Self {
+        <Self as Symbol>::new()
+    }
+}
+
+impl Nil {
+    #[inline(always)]
+    pub const fn new() -> Self {
+        Self
     }
 }
 
 impl<const C: char, Tail: Symbol> Default for Cons<C, Tail> {
+    #[inline(always)]
     fn default() -> Self {
         Self::new()
     }
 }
 
 impl fmt::Display for Nil {
+    #[inline(always)]
     fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
         Ok(())
     }
@@ -64,11 +95,68 @@ impl<const C: char, Tail: fmt::Debug> fmt::Debug for Cons<C, Tail> {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Chars<const C: char, Tail> {
+    used_c: bool,
+    tail: Tail,
+}
+
+impl<const C: char, Tail: Iterator<Item = char>> Iterator for Chars<C, Tail> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.used_c {
+            self.tail.next()
+        } else {
+            self.used_c = true;
+            Some(C)
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (lower, upper) = self.tail.size_hint();
+        if self.used_c {
+            return (lower, upper);
+        }
+        (
+            lower.saturating_add(1),
+            upper.and_then(|upper| upper.checked_add(1)),
+        )
+    }
+}
+
+impl<const C: char, Tail: DoubleEndedIterator<Item = char>> DoubleEndedIterator for Chars<C, Tail> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if let Some(c) = self.tail.next_back() {
+            return Some(c);
+        }
+        if self.used_c {
+            None
+        } else {
+            self.used_c = true;
+            Some(C)
+        }
+    }
+}
+
+impl<const C: char, Tail: ExactSizeIterator<Item = char>> ExactSizeIterator for Chars<C, Tail> {
+    fn len(&self) -> usize {
+        let len = self.tail.len();
+        if self.used_c {
+            len
+        } else {
+            len.saturating_add(1)
+        }
+    }
+}
+
+impl<const C: char, Tail: FusedIterator<Item = char>> FusedIterator for Chars<C, Tail> {}
+
 #[cfg(test)]
 mod tests {
     extern crate std;
 
-    use std::string::ToString;
+    use std::string::{String, ToString};
 
     use insta::{assert_debug_snapshot, with_settings};
 
@@ -86,7 +174,7 @@ mod tests {
     fn test_empty_symbol() {
         insta_assert!(<Symbol!("")>::new());
     }
-    
+
     #[test]
     fn test_display() {
         insta_assert!(<Symbol!("hello")>::new().to_string());
@@ -95,5 +183,15 @@ mod tests {
     #[test]
     fn test_debug() {
         insta_assert!(<Symbol!("hello")>::new());
+    }
+
+    #[test]
+    fn test_iter() {
+        insta_assert!(<Symbol!("hello")>::chars().collect::<String>());
+    }
+    
+    #[test]
+    fn test_iter_rev() {
+        insta_assert!(<Symbol!("hello")>::chars().rev().collect::<String>());
     }
 }
